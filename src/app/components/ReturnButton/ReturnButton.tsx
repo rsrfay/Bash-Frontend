ba92e1c43921ca4d5a34558d72f6ef1365dba90d
Russarin Eaimrittikrai <russarin9@gmail.com>
2024-10-18 22:28:14 +0700

    feat(ReturnButton): change css and customize font

--- src/app/components/ReturnButton/ReturnButton.tsx
+++ src/app/components/ReturnButton/ReturnButton.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import { AiOutlineLeft } from 'react-icons/ai';
-import { useRouter } from 'next/navigation';
-import styles from './ReturnButton.module.css';
+import React from "react";
+import { AiOutlineLeft } from "react-icons/ai";
+import { useRouter } from "next/navigation";
+import styles from "./ReturnButton.module.css";
 
 const ReturnButton: React.FC = () => {
   const router = useRouter();
 
   return (
-    <div className='px-4 flex items-center py-4 mt-2'>
-      <a onClick={() => router.back()} className={`${styles.returnButton} flex items-center space-x-2`}>
-        <AiOutlineLeft />
-        <span>Return</span>
+    <div className="px-4 flex items-center py-4 mt-2">
+      <a onClick={() => router.back()} className={styles.returnButton}>
+        <span className={styles.iconText}>
+          <AiOutlineLeft /> Return
+        </span>
       </a>
     </div>
   );
