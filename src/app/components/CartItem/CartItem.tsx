74f8e4bf18b526a2f7c22765fbaa5400313f8b55
Kongphob Wutthiumphol <kongphobkong852@hotmail.com>
2024-10-24 21:08:17 +0700

    add select cartItem
    add select cartItem then open item description

--- src/app/components/CartItem/CartItem.tsx
+++ src/app/components/CartItem/CartItem.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import styles from "./CartItem.module.css";
-import { FaTrash } from "react-icons/fa"; // Bin icon
+import { FaTrash } from "react-icons/fa";
+import { useRouter } from "next/navigation";
 
 type CartItemProps = {
+  id: number;
   itemName: string;
   itemDetails: string;
   quantity: number;
@@ -15,6 +17,7 @@ type CartItemProps = {
 };
 
 const CartCardItem: React.FC<CartItemProps> = ({
+  id,
   itemName,
   itemDetails,
   quantity,
@@ -25,8 +28,13 @@ const CartCardItem: React.FC<CartItemProps> = ({
   onRemove,
   isSelectMode,
 }) => {
+  const router = useRouter();
+
   return (
-    <div className={styles.cartItemContainer}>
+    <div
+      className={styles.cartItemContainer}
+      onClick={() => router.push(`/description/${id}`)}
+    >
       <div className={styles.imagePlaceholder}>
         <img src={image} className="card-image" />
       </div>
@@ -34,14 +42,36 @@ const CartCardItem: React.FC<CartItemProps> = ({
         <h3 className={styles.itemName}>{itemName}</h3>
         <p className={styles.itemDetails}>{itemDetails}</p>
         <div className={styles.quantityControl}>
-          <button onClick={onDecrease} className={styles.decrementButton}>-</button>
+          <button
+            onClick={(e) => {
+              e.stopPropagation();
+              onDecrease();
+            }}
+            className={styles.decrementButton}
+          >
+            -
+          </button>
           <span className={styles.quantity}>{quantity}</span>
-          <button onClick={onIncrease} className={styles.incrementButton}>+</button>
+          <button
+            onClick={(e) => {
+              e.stopPropagation();
+              onIncrease();
+            }}
+            className={styles.incrementButton}
+          >
+            +
+          </button>
         </div>
       </div>
       <div className={styles.itemPrice}>
         {isSelectMode ? (
-          <div className={styles.removeButton} onClick={onRemove}>
+          <div
+            className={styles.removeButton}
+            onClick={(e) => {
+              e.stopPropagation();
+              onRemove();
+            }}
+          >
             <FaTrash className={styles.trashIcon} />
           </div>
         ) : (
