77af9d06ca0ef726c03a8802b87a4098c4de60d0
Russarin Eaimrittikrai <russarin9@gmail.com>
2024-11-08 13:54:41 +0700

    fix(homepage): add component no result and fix css

---
+++ src/app/components/NoResult/NoResult.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Image from 'next/image';
+
+interface NoResultsProps {
+  textHeader: string;
+}
+
+const NoResults: React.FC<NoResultsProps> = ({ textHeader }) => {
+  return (
+    <div className="flex flex-col items-center mt-4 p-2">
+      <p className="text-[#674636] font-adlam text-xl m-2 text-center">{textHeader}</p>
+      <Image
+        src="/images/No-Result.png"
+        alt="No result"
+        width={200}
+        height={200}
+      />
+    </div>
+  );
+};
+
+export default NoResults;
