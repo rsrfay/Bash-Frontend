2f8032eab02b1918a6616c83fc3c0d9889c189ed
Chaninan Phetpangun <chaninan.phe@student.mahidol.ac.th>
2024-11-14 17:54:35 +0700

    handleSortChange

--- __tests__/HandleSortChange.test.tsx
+++ __tests__/HandleSortChange.test.tsx
@@ -13,15 +13,17 @@ interface Product {
     Tag: string[]; // Add this line for tags
   }
 
-  
+  describe("handleSortChange Function", () => {
 test("sorts products by price in ascending order", () => {
     const products: Product[] = [
         { id: 1, name: "Dirty", description: "A rich espresso shot served over cold milk, resulting in a bold flavor contrast.", hotPrice: "85", coldPrice: "90", category: "Drink", TypeOfDrinks: "Hot/Cold", isRecommended: true, image: "latte.png", Tag: ["Coffee","Recommend"]},
         { id: 2, name: "Americano", description: "An Americano is made by diluting an espresso shot with hot water for a smooth, robust coffee.", hotPrice: "55", coldPrice: "60", category: "Drink", TypeOfDrinks: "Hot/Cold", isRecommended: false, image: "americano.png", Tag: ["Coffee"]},
         { id: 3, name: "Latte", description: "A smooth blend of espresso and steamed milk with a creamy finish.", hotPrice: "60", coldPrice: "65", category: "Drink", TypeOfDrinks: "Hot/Cold", isRecommended: false, image: "latte.png", Tag: ["Coffee", "Milk"]}
     ];
-    const sortedProducts = handleSortChange(products, "Price Low to High");
-    expect(sortedProducts[0].id).toBe(2);
-    expect(sortedProducts[1].id).toBe(1);
-    expect(sortedProducts[2].id).toBe(3);
+    const sortedProducts = handleSortChange(products, "Invalid value");
+    console.log(sortedProducts)
+    expect(sortedProducts[0].id).toBe(2); // Expected to be the cheapest (Americano)
+    expect(sortedProducts[1].id).toBe(3); // Expected to be the next cheapest (Latte)
+    expect(sortedProducts[2].id).toBe(1); // Expected to be the most expensive (Dirty)
   });
+});
